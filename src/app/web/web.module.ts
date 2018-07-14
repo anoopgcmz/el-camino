1310013dc4089a8a890312fe1e2b40b73a7190f0
Anoop G <mr.anoopg@gmail.com>
2018-07-14 16:49:08 +0400

    created web module

---
+++ src/app/web/web.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { SharedModule } from '../shared/shared.module';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent }
+]
+
+@NgModule({
+  imports: [
+    SharedModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [HomeComponent]
+})
+export class WebModule { }
